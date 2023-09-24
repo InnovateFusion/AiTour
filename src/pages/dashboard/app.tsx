15e3b536072147322f5ee8fe11c1cc7d86d704fc
Bisrat Kebere <kiyyakebere@gmail.com>
2023-09-24 14:34:49 +0300

    first commit

---
+++ src/pages/dashboard/app.tsx
@@ -0,0 +1,68 @@
+// next
+import Head from 'next/head';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Container, Grid, Stack, Button } from '@mui/material';
+// auth
+import { useAuthContext } from '../../auth/useAuthContext';
+// layouts
+import DashboardLayout from '../../layouts/dashboard';
+// _mock_
+import {
+  _appFeatured,
+  _appAuthors,
+  _appInstalled,
+  _appRelated,
+  _appInvoices,
+} from '../../_mock/arrays';
+// components
+import { useSettingsContext } from '../../components/settings';
+// sections
+import {
+  AppWidget,
+  AppWelcome,
+  AppFeatured,
+  AppNewInvoice,
+  AppTopAuthors,
+  AppTopRelated,
+  AppAreaInstalled,
+  AppWidgetSummary,
+  AppCurrentDownload,
+  AppTopInstalledCountries,
+} from '../../sections/@dashboard/general/app';
+// assets
+import { SeoIllustration } from '../../assets/illustrations';
+
+// ----------------------------------------------------------------------
+
+GeneralAppPage.getLayout = (page: React.ReactElement) => <DashboardLayout>{page}</DashboardLayout>;
+
+// ----------------------------------------------------------------------
+
+export default function GeneralAppPage() {
+  const { user } = useAuthContext();
+
+  const theme = useTheme();
+
+  const { themeStretch } = useSettingsContext();
+
+  return (
+    <>
+      <Head>
+        <title> Home | AiTour</title>
+      </Head>
+
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8}>
+            
+          </Grid>
+
+          <Grid item xs={12} md={4}>
+
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
